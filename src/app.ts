03da826486b59597348cce5b1d6f2ada36f85baf
Mateusz <66961646+Regnar110@users.noreply.github.com>
2023-01-28 22:20:52 +0100

    code implementation

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-// Code goes here!
+import axios from "axios"
+
+const form = document.querySelector("form") as HTMLFormElement
+const addressInput = document.getElementById("address") as HTMLInputElement
+const googleApiKey = "AIzaSyBDz3bhbskJLQ4BElZxjJQSQC8iUMGVl4Q"
+
+const searchAdressHandler = (event: SubmitEvent) => {
+    event.preventDefault();
+    const enteredAdress = addressInput.value;
+
+    //send this to googles API
+
+    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAdress)}&key=${googleApiKey}`)
+    .then(res => {
+        console.log(res)
+    })
+    .catch(err => console.log(err))
+}
+
+form.addEventListener("submit", searchAdressHandler);
